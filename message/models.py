6a5193c0d08ea263c09d0651156fd1f8d3f88190
Muhammad Usama <81771131+muhammadusama6@users.noreply.github.com>
2023-04-10 15:53:14 +0500

    updated commit

--- message/models.py
+++ message/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 
 class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     text = models.TextField(default='')
     seen = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    sender_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    updated_at = models.DateTimeField(auto_now=True)
