129738c1c2a16a93e86514d79d352191adab292e
Muhammad Usama <81771131+muhammadusama6@users.noreply.github.com>
2023-04-10 14:42:51 +0500

    updated commit

--- message/views.py
+++ message/views.py
@@ -1,20 +1,13 @@
 from rest_framework import viewsets, permissions, status
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.admin import User
 from rest_framework.response import Response
-
 from message.models import Message
 from message.serializers import MessageSerializer
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet for handling CRUD operations for messages.
-    """
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         sender_id = self.request.user.id
@@ -26,9 +19,6 @@ class MessageViewSet(viewsets.ModelViewSet):
         return Message.objects.filter(sender_name=user) | Message.objects.filter(receiver_name=user)
 
     def destroy(self, request, *args, **kwargs):
-        """
-        Override delete method to allow only the sender to delete the message.
-        """
         instance = self.get_object()
         if instance.sender_name == self.request.user:
             self.perform_destroy(instance)
