9fa1c174d7b5df896b85272143781c7c9293ff03
Muhammad Usama <81771131+muhammadusama6@users.noreply.github.com>
2023-04-17 14:07:51 +0500

    updated commit

--- message/views.py
+++ message/views.py
@@ -9,20 +9,20 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
-    def create(self, request, *args, **kwargs):
-        if request.data['sender'] == '' or not request.data['sender']:
-            # request.data['sender'] = request.user
-            data = request.data.copy()
-            data['sender'] = request.user.id
-        serializer = self.serializer_class(data=data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(data=serializer.data)
+    # def create(self, request, *args, **kwargs):
+    #     if request.data['sender'] == '' or not request.data['sender']:
+    #         # request.data['sender'] = request.user
+    #         data = request.data.copy()
+    #         data['sender'] = request.user.id
+    #     serializer = self.serializer_class(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #     return Response(data=serializer.data)
 
-    # def perform_create(self, serializer):
-    #     sender = self.request.user
-    #     # sender = User.objects.get(id=sender_id)
-    #     serializer.save(sender=sender)
+    def perform_create(self, serializer):
+        sender_id = self.request.user.id
+        sender = User.objects.get(id=sender_id)
+        serializer.save(sender=sender)
 
     def get_queryset(self):
         user = self.request.user
